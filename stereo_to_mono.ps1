bfd77a2cc64e3d35e913f40fd4ef79e457cc6cc4
Coder747 <ayrefaie@live.com>
2021-04-08 22:21:31 +0200

    updates

--- stereo_to_mono.ps1
+++ stereo_to_mono.ps1
@@ -1,6 +1,6 @@
-﻿$Audio_Path = "C:\Users\Admin-Group11\Desktop\CNN\WAV\B\"
-$Output_Path_top = "C:\Users\Admin-Group11\Desktop\CNN\Mono_WAV\B\top\"
-$Output_Path_bot = "C:\Users\Admin-Group11\Desktop\CNN\Mono_WAV\B\bot\"
+﻿$Audio_Path = "C:\Users\Admin-Group11\Desktop\Sonar_CNN\User_WAV\DU\"
+$Output_Path_top = "C:\Users\Admin-Group11\Desktop\Sonar_CNN\User_Mono\DU\top\"
+$Output_Path_bot = "C:\Users\Admin-Group11\Desktop\Sonar_CNN\User_Mono\DU\bot\"
 
 Get-ChildItem $Audio_Path -Filter *.wav | ForEach-Object { ffmpeg -i $Audio_Path\$_ -map_channel 0.0.0 $Output_Path_top\$_`_top.wav -map_channel 0.0.1 $Output_Path_bot\$_`_bot.wav}
 
